f19b2efbc97da2d2edfaee0134ccca805656d628
rina <k@rina.fyi>
2026-03-24 18:01:58 +1000

    make_multiline_regex

--- tools/make_multiline_regex.py
+++ tools/make_multiline_regex.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+import sys
+
+# comment body that terminates at the next instance of `*/`
+non_nested_comment_body = r'(?:[^*]|[*]+[^*/])*[*]+[/]'
+
+string_without_comment_delimiter = \
+    r'(?:[^*/]|[*]+[^*/]|[/]+[^/*])*'
+
+def nested_comment_body(nestings: int) -> str:
+    if nestings == 0:
+        return non_nested_comment_body
+    return rf'(?:{string_without_comment_delimiter}|[/]+[*]{nested_comment_body(nestings-1)})*[*]+[/]'
+
+print('[/][*]' + nested_comment_body(int(sys.argv[1])))
