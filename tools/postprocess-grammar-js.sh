d2ce6de820c216f0514589d15619013eae933609
rina <k@rina.fyi>
2026-03-24 16:28:28 +1000

    codify ite fix

--- tools/postprocess-grammar-js.sh
+++ tools/postprocess-grammar-js.sh
@@ -11,25 +11,83 @@ cat <<EOF
 	*/
 
 /*
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at https://mozilla.org/MPL/2.0/.
-*/
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 const regexseq = (...args) => new RegExp("(?:" + args.map(x => x.source).join("") + ")");
 const regexrepeat = (arg) => new RegExp("(?:" + arg.source + ")*");
 const regexchoice = (...args) => new RegExp("(?:" + args.map(x => x.source).join("|") + ")");
 const regexoptional = arg => new RegExp("(?:" + arg.source + ")?");
 
+// In the vast majority of cases, the Boogie grammar is unambiguous and explicit,
+// so these precedences won't get used. HOWEVER, they DO get used in the presence
+// of "if-then-else" expressions to resolve conflicts between ITE and other infix
+// operators.
+//
+// For "if-then-else" expressions, the ITE is matched in AtomExpression, so the ITE
+// will be in the "_"-prefixed case. Specifying the EquivExpression as stronger
+// than _EquivExpression (for example) means that the <==> has stronger precedence
+// then ITE, so
+//
+//     if true then x else y <==> z
+//
+// is parsed as
+//
+//     if true then x else (y <==> z).
+//
+// Note that the rule without "_" is the real case (e.g., EquivExpression always has
+// at least one <==> operator), and the one with "_" is an EquivExpression or anything
+// with *stronger* precedence.
+const precedences_for_ite = $ => [
+  [$.EquivExpression, $._EquivExpression],
+  [$.ImpliesExpression, $._ImpliesExpression],
+  [$.LogicalExpression, $._LogicalExpression],
+  [$.RelationalExpression, $._RelationalExpression],
+  [$.BvTerm, $._BvTerm],
+  [$.Term, $._Term],
+  [$.Factor, $._Factor],
+  [$.Power, $._Power],
+  [$.IsConstructor, $._IsConstructor],
+  [$.UnaryExpression, $._UnaryExpression],
+  [$.CoercionExpression, $._CoercionExpression],
+  [$.ArrayExpression, $._ArrayExpression],
+];
+
+// In addition to the above rules which disambiguate an expression with an ITE,
+// we must also disambiguate between *multiple* infix repetitions. Again,
+// this is usually enforced by the grammar structure, so the ambiguity only arises
+// in the presence of ITE (and these are the only cases we must disambiguate).
+// We want to disambiguate the sentence
+//
+//     if true then false else x <==> y <==> z
+//
+// so that it gets parsed as
+//
+//     if true then false else (x <==> y <==> z)
+//
+// and NOT
+//
+//     (if true then false else (x <==> y)) <==> z.
+//
+// This is unlike the earlier ambiguity, because here we are already in the
+// EquivExpression rule. We just have to decide when to terminate the EquivExpression,
+// and this is a matter of associativity.
+//
+// To get the associativity we want, where ITEs are right associative, we insert
+// `prec.right` around the production for rules which may appear infix.
 
 EOF
 
-sed '
+sed -e '
   /ident:/,$ {
     s/seq(/regexseq(/g
     s/repeat(/regexrepeat(/g
     s/choice(/regexchoice(/g
     s/optional(/regexoptional(/g
   }
+' -e '
+  /name:/a\\n  \/\/ See comments above.\n  precedences: precedences_for_ite,
 ' \
   | sed 's/ \+$//g'
--- tools/ts-ebnf-fix-ll.py
+++ tools/ts-ebnf-fix-ll.py
@@ -3,6 +3,17 @@
 import sys
 import re
 
+# rules to be written into un-left-factored form. for example,
+#
+#     Term ::= Factor ("+" Factor)*
+#
+# becomes
+#
+#     _Term ::= _Factor | Term
+#     Term  ::= _Factor ("+" _Factor)+
+#
+# where the "_" indicates an expression of Term or stronger
+# precedence. the Term rule is changed to match exactly Term only.
 ll_rules = [
     'EquivExpression',
     'ImpliesExpression',
@@ -24,11 +35,24 @@ make_underscore = [
     'Expression'
 ]
 
+# make these rules right-precedence for ITE handling
+make_prec_right = [
+    "EquivExpression",
+    "ImpliesExpression",
+    "LogicalExpression",
+    "BvTerm",
+    "Term",
+    "Factor",
+    "CoercionExpression",
+    "ArrayExpression"
+]
+
 inp = sys.stdin.read()
 
 for ll in ll_rules + make_underscore:
     inp = re.sub(rf'\b{ll}\b', f'_{ll}', inp)
 
+# this runs after underscores have already been inserted
 def transform_line(line: str):
     rule_name = line.split('::=')[0].strip().lstrip('_')
     if rule_name in make_underscore or not line.startswith('  _'):
@@ -45,7 +69,10 @@ def transform_line(line: str):
     rest = re.sub(r'\*$', '+', rest)
     rest = re.sub(r'\?$', '', rest)
     yield f'{init} | {rule_name}'
-    yield f'  {rule_name} ::= {init_rule} ( {rest}'
+    if rule_name in make_prec_right:
+        yield f'  {rule_name} ::= >( {init_rule} ( {rest} )'
+    else:
+        yield f'  {rule_name} ::= {init_rule} ( {rest}'
 
 inp = '\n'.join(x for l in inp.splitlines() for x in transform_line(l))
 
